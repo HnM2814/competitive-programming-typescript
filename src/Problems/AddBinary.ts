dc56e965fef71b1568eb8945b02e777d60c7d44f
HERE\mistry <hiren.mistry@here.com>
2020-11-02 10:02:48 +0530

    Problem : Add Binary Strings - leetCode 67

--- src/Problems/AddBinary.ts
+++ src/Problems/AddBinary.ts
@@ -1,11 +1,20 @@
 /**
- * 
- * @param s1 
- * @param s2 
- * 
- *      1001
- *      1101
- *     10110
+ *      Given two binary strings, return their sum (also a binary string).
+        The input strings are both non-empty and contains only characters 1 or 0.
+
+        Example 1:
+        Input: a = "11", b = "1"
+        Output: "100"
+
+        Example 2
+        Input: a = "1010", b = "1011"
+        Output: "10101"
+        
+
+        Constraints:
+        - Each string consists only of '0' or '1' characters.
+        - 1 <= a.length, b.length <= 10^4
+        - Each string is either "0" or doesn't contain any leading zero.
  */
 const addBinaryString = (s1:string, s2:string) => {
     if(!s1 && s2) return s2;
